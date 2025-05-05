3b8288dc0a9882ed2b0303453055cbe7b8dd6dfa
Phillarmonkey <phillarmonkey@outlook.com>
2025-05-05 11:15:23 +0100

    Compliant with HttpKernel\Bundle\Bundle::build()

--- src/PIXicatoBundle.php
+++ src/PIXicatoBundle.php
@@ -3,13 +3,14 @@
 namespace Phillarmonic\PIXicato;
 
 use Phillarmonic\PIXicato\DependencyInjection\CompilerPass\TranslationCompilerPass;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Phillarmonic\PIXicato\DependencyInjection\PIXicatoExtension;
 
 class PIXicatoBundle extends Bundle
 {
-    public function getContainerExtension(): ?\Symfony\Component\DependencyInjection\Extension\ExtensionInterface
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new PIXicatoExtension();
@@ -17,7 +18,7 @@ class PIXicatoBundle extends Bundle
         return $this->extension;
     }
 
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
         $container->addCompilerPass(new TranslationCompilerPass());
